667750b3ca8867f28edc35b9b15e17a9c6282503
xxjiinn <22100448@handong.ac.kr>
2024-04-04 19:57:31 +0900

    update hw2

--- SecondSeminar/src/main/java/com/pard/SecondSeminar/User/controller/LogController.java
+++ SecondSeminar/src/main/java/com/pard/SecondSeminar/User/controller/LogController.java
@@ -1,23 +1,23 @@
-package com.pard.SecondSeminar.User.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Slf4j // log 찍는 어노테이션
-public class LogController {
-
-    @GetMapping("/log")
-    public String log(){
-        String name = "Spring";
-
-        log.info("name : {}", name);
-        log.warn("name : {}", name);
-        log.error("name : {}", name);
-        log.debug("name : {}", name);
-        log.trace("name : {}", name);
-
-        return "Hello";
-    }
-}
+//package com.pard.SecondSeminar.User.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Slf4j // log 찍는 어노테이션
+//public class LogController {
+//
+//    @GetMapping("/log")
+//    public String log(){
+//        String name = "Spring";
+//
+//        log.info("name : {}", name);
+//        log.warn("name : {}", name);
+//        log.error("name : {}", name);
+//        log.debug("name : {}", name);
+//        log.trace("name : {}", name);
+//
+//        return "Hello";
+//    }
+//}
